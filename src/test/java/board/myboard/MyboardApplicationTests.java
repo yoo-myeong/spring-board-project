2b7900e068a720d9504f7de8f50962efcd03dcfc
sceonn <yoou201@naver.com>
2022-12-22 22:57:58 +0900

    init : 프로젝트 세팅

---
+++ src/test/java/board/myboard/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package board.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
