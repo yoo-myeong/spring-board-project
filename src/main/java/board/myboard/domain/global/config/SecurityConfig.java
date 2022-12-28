c0263cce2559b1dee009db6e5b173469736b4935
sceonn <yoou201@naver.com>
2022-12-28 22:27:12 +0900

    feat: SecurityConfig 구현

--- src/main/java/board/myboard/domain/global/config/SecurityConfig.java
+++ src/main/java/board/myboard/domain/global/config/SecurityConfig.java
@@ -1,5 +1,35 @@
 package board.myboard.domain.global.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .formLogin().disable()//1 - formLogin 인증방법 비활성화
+                .httpBasic().disable()//2 - httpBasic 인증방법 비활성화(특정 리소스에 접근할 때 username과 password 물어봄)
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login", "/signUp","/").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
